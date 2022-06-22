42b990cfab9d32914ac48545726454dfe2cbf4b8
Jakub Faruga <kuba-faruga@o2.pl>
2022-06-23 01:08:02 +0200

    Modyfikacja skryptu CameraController

--- TowerDefense/Assets/Content/Camera/Scripts/CameraController.cs
+++ TowerDefense/Assets/Content/Camera/Scripts/CameraController.cs
@@ -1,39 +1,42 @@
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+namespace TowerDefense
 {
-    [field: SerializeField]
-    private Transform CameraSlot { get; set; }
-
-    [field: SerializeField]
-    private float MoveSpeed { get; set; }
-
-    private float XPosition { get; set; }
-    private float ZPosition { get; set; }
-
-    protected virtual void Awake()
+    public class CameraController : MonoBehaviour
     {
-        Initialize();
-    }
+        [field: SerializeField]
+        private Transform CameraSlot { get; set; }
 
-    protected virtual void Update()
-    {
-        MoveCamera();
-    }
+        [field: SerializeField]
+        private float MoveSpeed { get; set; }
 
-    private void Initialize()
-    {
-        XPosition = CameraSlot.position.x;
-        ZPosition = CameraSlot.position.z;
-    }
+        private float XPosition { get; set; }
+        private float ZPosition { get; set; }
 
-    private void MoveCamera()
-    {
-        XPosition += Input.GetAxis("Horizontal") * MoveSpeed * Time.deltaTime * -1;
-        ZPosition += Input.GetAxis("Vertical") * MoveSpeed * Time.deltaTime * -1;
+        protected virtual void Awake()
+        {
+            Initialize();
+        }
 
-        // XPosition = Mathf.Clamp(XPosition, MinX, MaxX);
+        protected virtual void Update()
+        {
+            MoveCamera();
+        }
 
-        CameraSlot.position = new Vector3(XPosition, CameraSlot.position.y, ZPosition);
+        private void Initialize()
+        {
+            XPosition = CameraSlot.position.x;
+            ZPosition = CameraSlot.position.z;
+        }
+
+        private void MoveCamera()
+        {
+            XPosition += Input.GetAxis("Horizontal") * MoveSpeed * Time.deltaTime * -1;
+            ZPosition += Input.GetAxis("Vertical") * MoveSpeed * Time.deltaTime * -1;
+
+            // XPosition = Mathf.Clamp(XPosition, MinX, MaxX);
+
+            CameraSlot.position = new Vector3(XPosition, CameraSlot.position.y, ZPosition);
+        }
     }
 }
