12accd6576984dfeebbbf51b42baa09406c0c5cf
Jakub Faruga <kuba-faruga@o2.pl>
2022-06-23 01:21:41 +0200

    Modyfikacja skryptu EnemySpawner

--- TowerDefense/Assets/Content/Enemy/Scripts/EnemySpawner.cs
+++ TowerDefense/Assets/Content/Enemy/Scripts/EnemySpawner.cs
@@ -18,6 +18,12 @@ namespace TowerDefense
 
         private List<Enemy> EnemySpawnedCollection { get; set; } = new List<Enemy>();
 
+        [field: SerializeField]
+        public KeyCode SpawnEnemyKey { get; set; } = KeyCode.K;
+
+        [field: SerializeField]
+        public KeyCode DestroyEnemyKey { get; set; } = KeyCode.L;
+
         protected virtual void Start()
         {
             SpawnEnemy();
@@ -25,15 +31,7 @@ namespace TowerDefense
 
         protected virtual void Update()
         {
-            if (Input.GetKey(KeyCode.K) == true)
-            {
-                SpawnEnemy();
-            }
-
-            if (Input.GetKey(KeyCode.L) == true)
-            {
-                DestroySpawnedEnemies();
-            }
+            HandleKeyboardInput();
         }
 
         private void SpawnEnemy()
@@ -53,6 +51,19 @@ namespace TowerDefense
             }
 
             EnemySpawnedCollection.Clear();
+        }
+
+        private void HandleKeyboardInput()
+        {
+            if (Input.GetKey(SpawnEnemyKey) == true)
+            {
+                SpawnEnemy();
+            }
+
+            if (Input.GetKey(DestroyEnemyKey) == true)
+            {
+                DestroySpawnedEnemies();
+            }
         }
     }
 }
