10ab7bae6a0151e7481b90b19920500aa915c569
College <xx>
2022-06-29 20:12:19 +0200

    Stworzenie całego ciała klasy TowerAttackData

--- TowerDefense/Assets/Scripts/TowerAttackData.cs
+++ TowerDefense/Assets/Scripts/TowerAttackData.cs
@@ -2,19 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName ="TowerAttackData", menuName = "ScriptableObjects/TowerAttackData")]
-public class TowerAttackData : ScriptableObject
+namespace TowerDefense
 {
-    [field:SerializeField]
-    public LayerMask EnemyLayerMask { get; set; }
-    [field: SerializeField]
-    public float AttackRadius { get; set; }
-    [field: SerializeField]
-    public float FireRate { get; set; }
+    [CreateAssetMenu(fileName = "TowerAttackData", menuName = "ScriptableObjects/TowerAttackData")]
+    public class TowerAttackData : ScriptableObject
+    {
+        [field: SerializeField]
+        public LayerMask EnemyLayerMask { get; set; }
+        [field: SerializeField]
+        public float AttackRadius { get; set; }
+        [field: SerializeField]
+        public float FireRate { get; set; }
 
-    [field: SerializeField]
-    public int Damage { get; set; }
+        [field: SerializeField]
+        public int Damage { get; set; }
 
-    //[field:SerializeField]
-    //public Projectile ProjectilePrefab { get; set; }
+        //[field:SerializeField]
+        //public Projectile ProjectilePrefab { get; set; }
+    }
 }
