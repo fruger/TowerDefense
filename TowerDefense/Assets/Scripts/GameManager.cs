d13e7a7459a9854dc67ea42b3c66d3bf7665d5d2
College <xx>
2022-06-29 20:04:48 +0200

    Stworzenie skryptu GameManager

---
+++ TowerDefense/Assets/Scripts/GameManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TowerDefense
+{
+    public class GameManager : SingletonMonoBehaviour<GameManager>
+    {
+
+    }
+}
