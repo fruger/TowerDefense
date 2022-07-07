f5a5d44d805bd9797da65a17621e46bb8a26d900
Jakub Faruga <kuba-faruga@o2.pl>
2022-07-08 00:15:33 +0200

    Stworzenie property ProjectilePrefab w skrypcie TowerAttackData

--- TowerDefense/Assets/Scripts/TowerAttackData.cs
+++ TowerDefense/Assets/Scripts/TowerAttackData.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace TowerDefense
@@ -17,7 +15,7 @@ namespace TowerDefense
         [field: SerializeField]
         public int Damage { get; set; }
 
-        //[field:SerializeField]
-        //public Projectile ProjectilePrefab { get; set; }
+        [field: SerializeField]
+        public Projectile ProjectilePrefab { get; set; }
     }
 }
