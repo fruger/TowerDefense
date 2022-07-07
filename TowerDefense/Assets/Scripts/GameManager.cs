fa58cec2662b14a0cf143864215f2b72d04b927f
Jakub Faruga <kuba-faruga@o2.pl>
2022-07-08 00:15:05 +0200

    Napisanie implementacji skryptu GameManager

--- TowerDefense/Assets/Scripts/GameManager.cs
+++ TowerDefense/Assets/Scripts/GameManager.cs
@@ -1,11 +1,66 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace TowerDefense
 {
     public class GameManager : SingletonMonoBehaviour<GameManager>
     {
+        [field: SerializeField]
+        public int CurrentCoins { get; private set; }
 
+        [field: SerializeField]
+        public float CurrentHealth { get; private set; }
+
+        [field: SerializeField]
+        private int MaxCoins { get; set; }
+
+        [field: SerializeField]
+        private float MaxHealth { get; set; }
+
+
+        public void TakeDamage(int damageValue)
+        {
+            CurrentHealth -= damageValue;
+
+            if (CurrentHealth <= 0)
+            {
+                OnGameLost();
+            }
+        }
+
+        public void AddCoins(int amount)
+        {
+            CurrentCoins += amount;
+            Debug.Log($"{amount} coins gained! You have {CurrentCoins} coins left");
+        }
+
+        public bool TryBuyTower(TowerController tower)
+        {
+            // if (CurrentCoins >= tower.Cost)
+            // {
+            //     SpendCoins(tower.Cost);
+            //     return true;
+            // }
+
+            return true;
+        }
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            CurrentHealth = MaxHealth;
+            CurrentCoins = MaxCoins;
+        }
+
+        private void SpendCoins(int amount)
+        {
+            CurrentCoins -= amount;
+            Debug.Log($"{amount} coins spent! You have {CurrentCoins} coins left");
+        }
+
+        private void OnGameLost()
+        {
+            Debug.Log("You have lost");
+        }
     }
 }
